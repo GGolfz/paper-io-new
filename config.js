d742b21310164df763e3ef37bb2a9d411ec58b95
GGolfz <wisarutgolf11@gmail.com>
2023-01-30 23:13:01 +0700

    update: size :sparkles:

--- config.js
+++ config.js
@@ -2,18 +2,17 @@ export const config = {
 	"dev": true,
 	"port": 8080,
 	"bots": 0,
-	"fps": 30
+	"fps": 60
 };
 
 export const consts = {
-	"GRID_COUNT": 100,
+	"GRID_COUNT": 300,
 	"CELL_WIDTH": 40,
 	"SPEED": 5,
 	"BORDER_WIDTH": 20,
 	"MAX_PLAYERS": 30,
-	"NEW_PLAYER_LAG": 60,
+	"NEW_PLAYER_LAG": 20,
 	"LEADERBOARD_NUM": 5,
 	"PREFIXES": "Angry Baby Crazy Diligent Excited Fat Greedy Hungry Interesting Japanese Kind Little Magic Naïve Old Powerful Quiet Rich Superman THU Undefined Valuable Wifeless Xiangbuchulai Young Zombie",
 	"NAMES": "Alice Bob Carol Dave Eve Francis Grace Hans Isabella Jason Kate Louis Margaret Nathan Olivia Paul Queen Richard Susan Thomas Uma Vivian Winnie Xander Yasmine Zach"
-
 };
